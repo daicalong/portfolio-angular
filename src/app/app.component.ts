fc0a5d401e5a223412f29d3ae1ba13e569157d8f
harry Nguyen <hpnguyen52@gmail.com>
2021-07-03 21:35:41 -0400

    added tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpContext, HttpHeaders, HttpParams } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
   time: Date = new Date();
+  url: string = 'https://api.airtable.com/v0/appWtGcSZHIQOOb33/Project%20List';
+  list: any;
+  options:any = {
+    headers: {
+      Authorization: 'Bearer keyZ5CpEswS5gndC1'
+    },
+    };
+  constructor(public http: HttpClient) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.http.get(this.url, this.options).subscribe(res => this.list = res);
+  }
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: true,
+      content: [
+        './src/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+};
