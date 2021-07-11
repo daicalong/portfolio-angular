24704cc0d6604eaf22a5de12d72aef7d224d7954
harry Nguyen <hpnguyen52@gmail.com>
2021-07-11 11:06:43 -0400

    undo tests

--- src/app/core/services/http/airtable-api/airtable-api.service.ts
+++ src/app/core/services/http/airtable-api/airtable-api.service.ts
@@ -3,13 +3,12 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Project, ProjectList } from 'src/app/shared/interfaces/project-list';
 import { environment } from 'src/environments/environment';
-require('dotenv').config();
 
 @Injectable({
   providedIn: 'root'
 })
 export class AirtableApiService {
-  token?: string = process.env.AIRTABLE_API_KEY;
+  token?: string = environment.airtableApiKey;
   url = 'https://api.airtable.com/v0/appWtGcSZHIQOOb33/ProjectList';
 
   constructor(public http: HttpClient) { }
